4c3cb32cd4d97cffec766f031d84dd34e0fe2674
Ubuntu <jhuang6625@JasonsWebServer2.l5ysl1xja43etik4acp0nodisb.bx.internal.cloudapp.net>
2020-02-05 04:11:14 +0000

    fixing port so the server can run again

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use(
 app.get("/*", defaultRoutes);
 
 
-var server = app.listen(3000, () => {
-    console.log("server running on port", 3000);
+var server = app.listen(8080, () => {
+    console.log("server running on port", 8080);
 });
 
 WebSocket.createWebSocketStream(server);
